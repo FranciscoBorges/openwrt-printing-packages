a5fd0ebb38c91c9592939dd18d2799c162081a7b
Francisco Borges <francisco.borges@gmail.com>
2013-05-21 15:54:02 +0200

    Fix problems identified in #3

--- opkg-install-printing-packages.sh
+++ opkg-install-printing-packages.sh
@@ -3,5 +3,5 @@ opkg install fontconfig ghostscript-fonts-std liberation-fonts ubuntu-fonts ghos
 opkg install cups-client cups-bjnp ghostscript gutenprint-cups openprinting-cups-filters
 opkg install avahi-daemon avahi-utils
 
-/etc/init.d/cups enable
+/etc/init.d/cupsd enable
 /etc/init.d/avahi-daemon enable
