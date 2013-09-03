c03762f6569f3b1468f1295c064840574bd7c8d4
Francisco Borges <francisco.borges@gmail.com>
2013-09-03 21:29:35 +0200

    Fix #6 remove libjpeg-turbo

--- opkg-remove-printing-packages.sh
+++ opkg-remove-printing-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-opkg remove 'cups*' --force-removal-of-dependent-packages
+opkg remove 'cups*' --force-removal-of-dependent-packages libjpeg-turbo
 opkg remove --force-removal-of-dependent-packages 'libcups*'
 opkg remove 'ghostscript*'
 opkg remove --force-removal-of-dependent-packages lcms2 libijs qpdf libfreetype fontconfig poppler
